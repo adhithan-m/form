e99bda9650e31b017a6bc0b80f569cb6847aabff
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-26 22:22:09 +0530

    form for react js practice.

---
+++ form.js
@@ -0,0 +1,11 @@
+login=document.getElementById("ven")
+login.setAttribute("onclick","venkat()")
+function venkat(){
+	alert("login successfully")
+}
+
+cancel=document.getElementById("van")
+cancel.setAttribute("onclick","adhi()")
+function adhi(){
+	alert("login Failed")
+}
